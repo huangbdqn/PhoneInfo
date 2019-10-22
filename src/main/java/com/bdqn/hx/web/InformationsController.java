a035bb32b8c40aac64ec7621876a9dd78134385e
huangbdqn <263993483@qq.com>
2019-10-22 19:24:58 +0800

    修改说明

--- src/main/java/com/bdqn/hx/web/InformationsController.java
+++ src/main/java/com/bdqn/hx/web/InformationsController.java
@@ -19,6 +19,13 @@ public class InformationsController {
     @Resource
     InformationsService informationsService;
 
+    /** 
+    * @Description: 阿发送到发撒旦法 
+    * @Param:  
+    * @return:  
+    * @Author: 
+    * @Date: 2019/10/22 
+    */
     @GetMapping("queryAll")
     public ResponseEntity<List<Informations>> queryAll(){
         List<Informations> list = informationsService.queryAll();
